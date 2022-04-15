215e55d49ce27f66ab06aee12aa9dbc552316bed
wen <201930390197@mail.scut.edu.cn>
2022-04-15 20:22:21 +0800

    tanwen commit to master

--- C.c
+++ C.c
@@ -3,10 +3,17 @@
 int main(){
     int a;
     int b;
+    printf("hello world\n");
     printf("%d\n",a);
     printf("%d\n",b);
     int c;
     int d;
+    printf("intsert sentence\n");
     printf("%d\n",c);
     printf("%d\n",d);
+    int e;
+    printf("lalala\n");
+    int f;
+    printf("%d\n",e);
+    printf("%d\n",f);
 }
